3a73a635d3104dabdedc21cc85584be9033c6947
AegisofSins <113641383+AegisofSins@users.noreply.github.com>
2023-01-14 17:07:42 -0800

    Made base files

---
+++ script.js
@@ -0,0 +1,15 @@
+//1. Create 100rem x 100rem HTML container
+//2. Create 
+const main = document.getElementById('main');
+const grid = document.createElement('div');
+
+
+//CONTAINER
+main.appendChild(grid);
+grid.classList.add('grid');
+
+
+
+
+
+
